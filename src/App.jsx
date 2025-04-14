71b4c377cca68bfb2bcfbbfd9d14f82392a37d8d
Ricardo Brites <ricardo.brites.work@gmail.com>
2025-04-14 17:48:30 +0100

    Modified Spacing
    Modified About section to not be confined;
    Fixed a visual issue where the about section would have a small scrollable space;
    Modified the header, now its floating to avoid spacing issues with the rest of the page (padding can be used if needed on a per-element basis);

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import './App.css'
 import Header from './Components/Header/HeaderComponent'
 import About from './Components/AboutSection/AboutComponent'
 
-function App() {
+export default function App() {
   return (
     <div>
-      <Header></Header>
-      
-      <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
-        <section className="h-screen snap-start flex items-center justify-center bg-zinc-100 dark:bg-zinc-900">
+        <header className="fixed top-0 left-0 w-full z-50">
+          <Header></Header>
+        </header>
+      <div className="h-screen overflow-y-scroll snap-y snap-mandatory flex flex-col">
+        <section className=''>
           <About></About>
+        </section>
+        <section >
+          
         </section>
       </div>
     </div>
   )
 }
-
-export default App
--- src/Components/AboutSection/AboutComponent.jsx
+++ src/Components/AboutSection/AboutComponent.jsx
@@ -4,12 +4,12 @@ export default function About() {
         id="about"
         className="min-h-screen snap-start flex items-center justify-center bg-zinc-100 dark:bg-zinc-900 px-6"
       >
-        <div className="max-w-3xl text-center">
+        <div className="text-center">
           <h2 className="text-4xl font-bold text-zinc-900 dark:text-zinc-100 mb-4">
            Hi!
           </h2>
           <p className="text-lg text-zinc-700 dark:text-zinc-300 leading-relaxed">
-            <p>I'm Ricardo Brites, a Gameplay Programmer with a curiosity problem.</p> <br />
+            <p>I'm Ricardo Brites, a Gameplay Programmer with a curiosity problem (I have too much of it).</p> <br />
             <p>I'm currently looking to relocate to the US for work — though I don't have a visa just yet.</p> <br />
             <p>Please check out the sections below for more information.</p>
           </p>
