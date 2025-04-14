befe5e9d91b9189ea90d4160aac906f2687bce7b
Ricardo Brites <ricardo.brites.work@gmail.com>
2025-04-14 17:20:11 +0100

    Added an "About" Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from './Components/Header/HeaderComponent'
+import About from './Components/AboutSection/AboutComponent'
 
 function App() {
   return (
     <div>
       <Header></Header>
+      
+      <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+        <section className="h-screen snap-start flex items-center justify-center bg-zinc-100 dark:bg-zinc-900">
+          <About></About>
+        </section>
+      </div>
     </div>
   )
 }
---
+++ src/Components/AboutSection/AboutComponent.jsx
@@ -0,0 +1,20 @@
+export default function About() {
+    return (
+      <section
+        id="about"
+        className="min-h-screen snap-start flex items-center justify-center bg-zinc-100 dark:bg-zinc-900 px-6"
+      >
+        <div className="max-w-3xl text-center">
+          <h2 className="text-4xl font-bold text-zinc-900 dark:text-zinc-100 mb-4">
+           Hi!
+          </h2>
+          <p className="text-lg text-zinc-700 dark:text-zinc-300 leading-relaxed">
+            <p>I'm Ricardo Brites, a Gameplay Programmer with a curiosity problem.</p> <br />
+            <p>I'm currently looking to relocate to the US for work — though I don't have a visa just yet.</p> <br />
+            <p>Please check out the sections below for more information.</p>
+          </p>
+        </div>
+      </section>
+    );
+  }
+  
