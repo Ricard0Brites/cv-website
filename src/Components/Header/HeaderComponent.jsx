bc5cd0d4fdb4c00f4ab361bed488edda5c030e37
Ricardo Brites <ricardo.brites.work@gmail.com>
2025-04-14 16:32:16 +0100

    Modified Header Colors

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -14,6 +14,7 @@ export default function Header() {
 
     const Dark_PrimarBorderColor = 'border-zinc-600';
     const Dark_PrimaryHoverBGColor = 'bg-zinc-500';
+    const Dark_SecondaryBorderColor = 'border-zinc-600';
 
 
     /* Light Coloring */
@@ -24,12 +25,12 @@ export default function Header() {
     const Light_PrimaryBGHoverColor = 'bg-zinc-300';
     const Light_PrimaryStrokeColor = 'stroke-zinc-300';
     const Light_SecondaryTextColor = 'text-zinc-300';
-    const Light_SecondaryBorderColor = 'border-zinc-300';
+    const Light_SecondaryBorderColor = 'border-zinc-100';
 
 
     /* Icons */
-    const MoonIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className={`size-6 ${Light_PrimaryStrokeColor}`}><path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" /></svg>;  
-    const SunIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-6 ${Dark_PrimaryStrokeColor}`}><path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>;
+    const MoonIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} className={`size-6 ${Light_PrimaryStrokeColor}`}><path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" /></svg>;  
+    const SunIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className={`size-6 ${Dark_PrimaryStrokeColor}`}><path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>;
 
     
   // Get initial dark mode state from localStorage or system preference
@@ -52,7 +53,7 @@ export default function Header() {
   }, [darkMode]);  // When darkMode changes, update the class and localStorage
 
   return (
-    <header className= {`sticky top-0 z-50 ${Light_PrimaryBGColor} dark:${Dark_PrimarBGColor} backdrop-blur-md border-b ${Light_SecondaryBorderColor} dark:${Dark_PrimarBorderColor} shadow-sm`}>
+    <header className= {`sticky top-0 z-50 ${Light_PrimaryBGColor} dark:${Dark_PrimarBGColor} backdrop-blur-md border-b ${Light_SecondaryBorderColor} dark:${Dark_SecondaryBorderColor} shadow-sm`}>
       <div className="max-w-10xl mx-auto px-4 py-1 flex justify-between items-center">
         <div>
           <h1 className= {`text-xl font-bold ${Dark_PrimaryTextColor} dark:${Light_PrimaryTextColor}`}>
